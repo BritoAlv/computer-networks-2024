bc96fbdb041520a69f739a13bbdd869aa0f7a8b1
Javier Lima <javi.limashelby@gmail.com>
2024-04-11 00:27:33 -0400

    feat: Allow multiple directories creation

--- mkd.go
+++ mkd.go
@@ -1,9 +1,23 @@
 package main
 
+import "strings"
+
 func (cs *CommandsStruct) MKD(input string) (string, error) {
-	response, err := writeAndreadOnMemory(cs.connection, []byte("MKD "+input+"\r\n"))
-	if err != nil {
-		return "", err
+
+	directories := strings.Split(input, "/")
+	var response []byte
+	var err error
+	currentDirectory := directories[0]
+
+	for i := 0; i < len(directories); i++ {
+		if i > 0 {
+			currentDirectory += "/" + directories[i]
+		}
+
+		response, err = writeAndreadOnMemory(cs.connection, []byte("MKD "+currentDirectory+"\r\n"))
+		if err != nil {
+			return "", err
+		}
 	}
 
 	return ParseFTPCode(string(response)[0:3])
