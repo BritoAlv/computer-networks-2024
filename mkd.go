5960c539526be23a7945217f37bfb1ed0faa705d
Javier Lima <javi.limashelby@gmail.com>
2024-04-11 00:12:38 -0400

    fix: Do not split input

--- mkd.go
+++ mkd.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"errors"
-	"strings"
-)
-
 func (cs *CommandsStruct) MKD(input string) (string, error) {
-	split := strings.Split(input, " ")
-
-	if len(split) != 1 {
-		return "", errors.New("invalid input")
-	}
-
-	response, err := writeAndreadOnMemory(cs.connection, []byte("MKD "+split[0]+"\r\n"))
+	response, err := writeAndreadOnMemory(cs.connection, []byte("MKD "+input+"\r\n"))
 	if err != nil {
 		return "", err
 	}
