6a73e2486c5560cd0e963da7969899415bab76c1
Javier Lima <javi.limashelby@gmail.com>
2024-04-15 01:20:02 -0400

    feat: Apply components on main

---
+++ FtpGUI/src/scripts/main.js
@@ -0,0 +1,6 @@
+import { connectionSetupComponent } from "./components/connectionSetup.js";
+import { connectionStatusComponent } from "./components/connectionStatus.js";
+
+// Load required components
+connectionSetupComponent();
+connectionStatusComponent();
