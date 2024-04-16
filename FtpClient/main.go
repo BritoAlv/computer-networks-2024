862679cac435f1bb1e231fae9bdf1dfd5bbd6a47
BritoAlv <gomzalezbrito@gmail.com>
2024-04-16 09:04:50 -0400

    separate concerns

--- FtpClient/main.go
+++ FtpClient/main.go
@@ -9,11 +9,34 @@ import (
 	"strings"
 )
 
+func execute_command(command string, X *CommandsStruct) {
+	command = strings.Split(command, "\n")[0]
+	parts := strings.Split(command, " ")
+	if len(parts) == 0 {
+		fmt.Println("no command written")
+		return 
+	}
+	command_name := parts[0]
+	method := reflect.ValueOf(X).MethodByName(strings.ToUpper(command_name))
+
+	if !method.IsValid() {
+		fmt.Println("command not implemented by the client")
+		return
+	}
+	resultCommand := method.Call([]reflect.Value{reflect.ValueOf(strings.TrimSpace(command[len(command_name):]))})
+
+	result, _ := resultCommand[0].Interface().(string)
+	
+	if !resultCommand[1].IsNil() {
+		resultError, _ := resultCommand[1].Interface().(error)
+		fmt.Println(resultError.Error())
+	}
+	fmt.Println("Command Says : \n" + result)
+}
+
 func main() {
 	var X CommandsStruct
-
 	ftptouse := Scene
-
 	conn, err := net.Dial("tcp", strings.TrimSpace(ftptouse.ip)+":"+strings.TrimSpace(ftptouse.port))
 	if err != nil {
 		fmt.Println("Connection can't be established: ")
@@ -33,27 +56,6 @@ func main() {
 		fmt.Print(">> ")
 		reader := bufio.NewReader(os.Stdin)
 		command, _ := reader.ReadString('\n')
-		command = strings.Split(command, "\n")[0]
-		parts := strings.Split(command, " ")
-		if len(parts) == 0 {
-			fmt.Println("Wrongg")
-			continue
-		}
-		command_name := parts[0]
-		method := reflect.ValueOf(&X).MethodByName(strings.ToUpper(command_name))
-
-		if !method.IsValid() {
-			fmt.Println("help maybe ?")
-			continue
-		}
-		resultCommand := method.Call([]reflect.Value{reflect.ValueOf(strings.TrimSpace(command[len(command_name):]))})
-
-		result, _ := resultCommand[0].Interface().(string)
-
-		fmt.Println("Command Says : \n" + result)
-		if !resultCommand[1].IsNil() {
-			resultError, _ := resultCommand[1].Interface().(error)
-			fmt.Println("	Error : " + resultError.Error())
-		}
+		execute_command(command, &X)
 	}
 }
