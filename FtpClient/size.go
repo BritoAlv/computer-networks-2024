35e9a0dbe9ab1d423189f615f00f15272d02357f
BritoAlv <gomzalezbrito@gmail.com>
2024-04-16 06:24:40 -0400

    handle case where size is not implemented

--- FtpClient/size.go
+++ FtpClient/size.go
@@ -8,10 +8,7 @@ import (
 func (cs *CommandsStruct) SIZE(arg string) (string, error) {
 	result, err := writeAndreadOnMemory(cs.connectionConfig, "SIZE " + arg)
 	if err != nil {
-		if result[0:3] == "500" {
-			return strconv.Itoa(no_size), nil
-		}
-		return "0", err
+		return strconv.Itoa(no_size), nil
 	}
 	sizeStr := string(result)
 	sizeStr = strings.Split(sizeStr, " ")[1]
